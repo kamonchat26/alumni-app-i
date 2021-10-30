a40d8c96372538550727d7596bcb50853800195b
DESKTOP-LQO71O4\User <kamonchat2607@gmail.com>
2021-10-31 02:30:42 +0700

    edit component aboutus contactus and faq ,and have installation ng-bootstrap for angular

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
-export class FaqComponent implements OnInit {
-
+export class FaqComponent implements OnInit{
+  public isCollapsed = true;
+  public isCollapsedtwo = true;
+  public isCollapsedthree = true;
+  public isCollapsedfour = true;
+  public isCollapsedfive = true;
+  
+  inputQuestion: any;
   constructor() { }
 
   ngOnInit(): void {
-    alert('ยินดีต้อนรับ');
+  }
+  onSubmitQuestion() {
+    if (this.inputQuestion == "0" ) {
+      Swal.fire({
+        title: 'เกิดข้อผิดพลาด',
+        text: 'กรุณากรอกคำถาม',
+        icon: 'error',
+        confirmButtonText: 'close'
+      })
+    } else {
+      Swal.fire({
+        title: 'บันทึกคำถามของคุณแล้ว',
+        text: ' ขอบคุณค่ะ ',
+        icon: 'success',
+        confirmButtonText: 'close'
+      })
+    }
   }
 
 }
